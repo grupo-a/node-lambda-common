bd2be9e7e0f131a4a018ccfbafb84992aa653b29
Jonatas da Costa Saraiva <jsaraiva@grupoa.com.br>
2019-10-25 14:45:26 -0300

    Log erro on JSON

--- src/helpers/log.helper.js
+++ src/helpers/log.helper.js
@@ -1,24 +1,33 @@
-const infoEnable  = process.env.LOG_INFO_ENABLE;
+const infoEnable = process.env.LOG_INFO_ENABLE;
 const errorEnable = process.env.LOG_ERROR_ENABLE;
-const warnEnable  = process.env.LOG_WARN_ENABLE;
+const warnEnable = process.env.LOG_WARN_ENABLE;
 
-const error = function(functionName, errorMessage, err){
-    if (errorEnable)
-        console.error(`${functionName} - ${errorMessage}`, err);
+const error = (functionName, errorMessage, err) => {
+    if (errorEnable) {
+        const errorToLog = {
+            name: err.name,
+            message: err.message,
+            stack: err.stack,
+            ...err
+        };
+        console.error(`${functionName} - ${errorMessage}`, JSON.stringify(errorToLog));
+    }
 }
 
-const info = function(functionName, message){
-    if (infoEnable)
+const info = (functionName, message) => {
+    if (infoEnable) {
         console.info(`${functionName} - ${message}`);
+    }
 }
 
-const warn = function(functionName, message){
-    if (warnEnable)
+const warn = (functionName, message) => {
+    if (warnEnable) {
         console.warn(`${functionName} - ${message}`);
+    }
 }
 
 module.exports = {
     error,
     info,
     warn
-  };
+};
