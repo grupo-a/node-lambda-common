c45af54ae587f8421564ccdfaa24efe2975fd350
Jonatas da Costa Saraiva <jsaraiva@grupoa.com.br>
2019-11-08 13:53:15 -0300

    Change logger

--- src/helpers/log.helper.js
+++ src/helpers/log.helper.js
@@ -1,33 +1,43 @@
-const infoEnable = process.env.LOG_INFO_ENABLE;
+const infoEnable  = process.env.LOG_INFO_ENABLE;
 const errorEnable = process.env.LOG_ERROR_ENABLE;
-const warnEnable = process.env.LOG_WARN_ENABLE;
+const warnEnable  = process.env.LOG_WARN_ENABLE;
 
-const error = (functionName, errorMessage, err) => {
+const error = (functionName, error, contexMessage) => {
     if (errorEnable) {
-        const errorToLog = {
-            name: err.name,
-            message: err.message,
-            stack: err.stack,
-            ...err
-        };
-        console.error(`${functionName} - ${errorMessage}`, JSON.stringify(errorToLog));
+        console.error(JSON.stringify({
+            functionName,
+            contexMessage,
+            error: {
+                message: error.message,
+                ...error,
+                stack: error.stack
+            },
+        }));
     }
 }
 
-const info = (functionName, message, detailsObject) => {
+const info = (functionName, contexMessage, detailsObject) => {
     if (infoEnable) {
         if (detailsObject) {
-            console.info(`${functionName} - ${message}`, JSON.stringify(detailsObject));
+            console.info(JSON.stringify({
+                functionName,
+                contexMessage,
+                detailsObject
+            }));
         } else {
             console.info(`${functionName} - ${message}`);
         }
     }
 }
 
-const warn = (functionName, message, detailsObject) => {
+const warn = (functionName, contexMessage, detailsObject) => {
     if (warnEnable) {
         if (detailsObject) {
-            console.warn(`${functionName} - ${message}`, JSON.stringify(detailsObject));
+            console.warn(JSON.stringify({
+                functionName,
+                contexMessage,
+                detailsObject
+            }));
         } else {
             console.warn(`${functionName} - ${message}`);
         }
